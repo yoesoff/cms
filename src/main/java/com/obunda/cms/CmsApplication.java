7bf9779a893ea5686c8d925b26849eead4187c2c
Yusuf Ibrahim <mhyusufibrahim@gmail.com>
2020-04-15 00:31:06 +0700

    First commit!

---
+++ src/main/java/com/obunda/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package com.obunda.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
